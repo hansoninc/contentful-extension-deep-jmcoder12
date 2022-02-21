d4181c02a91f6d4edc6763b606fae17296983c94
dave.rodriguez <ohiodave@gmail.com>
2022-02-21 13:41:41 -0500

    Tweaks to extension to change default content "tag" to "[fr-CA]" and to prepend the tag reather than append it.

--- source/deep-copy2.js
+++ source/deep-copy2.js
@@ -68,7 +68,9 @@ async function createNewEntriesFromReferences (space, tag) {
 
   for (let entryId in references) {
     const entry = references[entryId]
-    if (entry.fields.title && entry.fields.title['en-US']) entry.fields.title['en-US'] = entry.fields.title['en-US'] + ' ' + tag
+    if (entry.fields.title && entry.fields.title['en-US']) {
+      entry.fields.title['en-US'] = `${tag} ${entry.fields.title['en-US']}`;
+    }
     const newEntry = await createEntry(space, entry.sys.contentType.sys.id, { fields: entry.fields })
     newReferenceCount++
     newEntries[entryId] = newEntry
